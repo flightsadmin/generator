4da7afac38d3248205aef5702be18c7a8ba4b6c0
George Chitechi <george@flightasdmin.com>
2021-04-29 09:16:49 +0400

    Initial commit

--- src/Install/app/Views/layouts/footer.php
+++ src/Install/app/Views/layouts/footer.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-	<strong>Copyright &copy; 2018-<?php echo date('Y'); ?> <a href="https://adminlte.io">Flight Admin</a>.</strong>
+	<strong>Copyright &copy; 2018-<?php echo date('Y'); ?> <a href="https://adminlte.io">AdminLTE</a>.</strong>
 	All rights reserved.
 		<div class="float-right d-none d-sm-inline-block">
 		  <b>Version</b> 3.1.0
